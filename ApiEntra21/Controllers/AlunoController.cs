010583f582fc5d2011ea6280fd10765fef59e3fe
Josepphh0 <zezinhhoo@gmail.com>
2025-07-29 21:26:54 -0300

    Adicionar arquivos de projeto.

---
+++ ApiEntra21/Controllers/AlunoController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Modelo.Domain;
+
+namespace ApiEntra21.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : Controller
+    {
+        [HttpGet("BuscarDadosAluno/{id}")]
+        public IActionResult BuscarDadosAluno(int id)
+        {
+            try
+            {
+                Aluno aluno = new Aluno();
+                aluno.Id = 1;
+                aluno.Nome = "João da Silva";
+                aluno.Idade = 20;
+                aluno.Cep = "12345-678";
+
+                return Ok(aluno);
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+           
+        }
+    }
+}
